f554aca33792c07f82fe5718ebb276ac6b8ff278
Adel Yasser <devadel@tutamail.com>
2025-12-30 18:21:14 +0200

    deleted problemetic files ending with . and .pyc

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.accounts.urls')),
+    #path('', include('apps.accounts.urls')),
     path('wallet/', include('apps.wallet.urls')),     
     path('bets/', include('apps.bets.urls')),        
 ]
